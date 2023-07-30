aeac4ef75db7ef1eec4a526166fcc57979a4ac9f
Bapparaj sk <bapparajsk97@gmail.com>
2023-07-30 10:31:37 +0530

    music

--- script.js
+++ script.js
@@ -1,112 +1,112 @@
 const arijit = [
     {
         name: "Chhod Diya",
-        img: "../../songs/Arijitimg/Chhod Diya.jpg",
-        path: "../../songs/ArijitSong/Chhod Diya (Lyrics).mp3"
+        img: "songs/Arijitimg/Chhod Diya.jpg",
+        path: "songs/ArijitSong/Chhod Diya (Lyrics).mp3"
     },
     {
         name: "O sudh budh khoyi hai khoyi Maine",
-        img: "../../songs/Arijitimg/O sudh budh khoyi hai khoyi Maine.jpg",
-        path: "../../songs/ArijitSong/O sudh budh khoyi hai khoyi Maine.mp3"
+        img: "songs/Arijitimg/O sudh budh khoyi hai khoyi Maine.jpg",
+        path: "songs/ArijitSong/O sudh budh khoyi hai khoyi Maine.mp3"
     },
     {
         name: "phir-aur-kya-chahiye",
-        img: "../../songs/Arijitimg/phir-aur-kya-chahiye.jpg",
-        path: "../../songs/ArijitSong/Phir Aur Kya Chahiye.mp3"
+        img: "songs/Arijitimg/phir-aur-kya-chahiye.jpg",
+        path: "songs/ArijitSong/Phir Aur Kya Chahiye.mp3"
     },
     {
         name: "Tera Fitoor Lyrical - Genius",
-        img: "../../songs/Arijitimg/Tera Fitoor Lyrical - Genius.jpg",
-        path: "../../songs/ArijitSong/Tera Fitoor Lyrical - Genius.mp3"
+        img: "songs/Arijitimg/Tera Fitoor Lyrical - Genius.jpg",
+        path: "songs/ArijitSong/Tera Fitoor Lyrical - Genius.mp3"
     },
     {
         name: "Tere Hawale (LYRICS)",
-        img: "../../songs/Arijitimg/Tere Hawale (LYRICS).jpg",
-        path: "../../songs/ArijitSong/Tere Hawale (LYRICS).mp3"
+        img: "songs/Arijitimg/Tere Hawale (LYRICS).jpg",
+        path: "songs/ArijitSong/Tere Hawale (LYRICS).mp3"
     }
 ];
 
 const Bollywood = [
     {
         name: "Kala Chashma Baar Baar Dekho",
-        img: "../../songs/Bollywood Hits img/Kala Chashma Baar Baar Dekho.jpg",
-        path: "../../songs/Bollywood Hits Song/Kala Chashma  Baar Baar Dekho.mp3"
+        img: "songs/Bollywood Hits img/Kala Chashma Baar Baar Dekho.jpg",
+        path: "songs/Bollywood Hits Song/Kala Chashma  Baar Baar Dekho.mp3"
     },
     {
         name: "Kamariya – Mitron",
-        img: "../../songs/Bollywood Hits img/Kar Gayi Chull - Kapoor & Sons.jpeg",
-        path: "../../songs/Bollywood Hits Song/Kar Gayi Chull - Kapoor & Sons.mp3"
+        img: "songs/Bollywood Hits img/Kar Gayi Chull - Kapoor & Sons.jpeg",
+        path: "songs/Bollywood Hits Song/Kar Gayi Chull - Kapoor & Sons.mp3"
     },
     {
         name: "London Thumakda",
-        img: "../../songs/Bollywood Hits img/London Thumakda.jpg",
-        path: "../../songs/Bollywood Hits Song/London Thumakda.mp3"
+        img: "songs/Bollywood Hits img/London Thumakda.jpg",
+        path: "songs/Bollywood Hits Song/London Thumakda.mp3"
     },
     {
         name: "Nashe Si Chadh Gayi",
-        img: "../../songs/Bollywood Hits img/Nashe Si Chadh Gayi.jpg",
-        path: "../../songs/Bollywood Hits Song/Nashe Si Chadh Gayi.mp3"
+        img: "songs/Bollywood Hits img/Nashe Si Chadh Gayi.jpg",
+        path: "songs/Bollywood Hits Song/Nashe Si Chadh Gayi.mp3"
     },
     {
         name: "Sauda Khara Khara Good Newwz",
-        img: "../../songs/Bollywood Hits img/Sauda Khara Khara Good Newwz.avif",
-        path: "../../songs/Bollywood Hits Song/Sauda Khara Khara Good Newwz.mp3"
+        img: "songs/Bollywood Hits img/Sauda Khara Khara Good Newwz.avif",
+        path: "songs/Bollywood Hits Song/Sauda Khara Khara Good Newwz.mp3"
     }
 ];
 
 const mysong = [
     {
         name: "vur yüreğim✘gangsta's paradise",
-        img: "../../songs/vur yüreğim✘gangsta img/vur.jpg",
-        path: "../../songs/vur yüreğim✘gangsta song/vur.mp3"
+        img: "songs/vur yüreğim✘gangsta img/vur.jpg",
+        path: "songs/vur yüreğim✘gangsta song/vur.mp3"
     },
     {
         name: "INSTASAMKA - Отключаю телефон",
-        img: "../../songs/vur yüreğim✘gangsta img/INSTASAMKA.jpeg",
-        path: "../../songs/vur yüreğim✘gangsta song/INSTASAMKA.mp3"
+        img: "songs/vur yüreğim✘gangsta img/INSTASAMKA.jpeg",
+        path: "songs/vur yüreğim✘gangsta song/INSTASAMKA.mp3"
     },
     {
         name: "Night Lovell - Polozhenie",
-        img: "../../songs/vur yüreğim✘gangsta img/Nightlove.jpg",
-        path: "../../songs/vur yüreğim✘gangsta song/Night Lovell.mp3"
+        img: "songs/vur yüreğim✘gangsta img/Nightlove.jpg",
+        path: "songs/vur yüreğim✘gangsta song/Night Lovell.mp3"
     },
     {
         name: "Xcho - Ты и Я (Tik Tok Remix) ",
-        img: "../../songs/vur yüreğim✘gangsta img/Xcho.jpg",
-        path: "../../songs/vur yüreğim✘gangsta song/Xcho.mp3"
+        img: "songs/vur yüreğim✘gangsta img/Xcho.jpg",
+        path: "songs/vur yüreğim✘gangsta song/Xcho.mp3"
     },
     {
         name: "CVRTOON - İzmir Marş",
-        img: "../../songs/vur yüreğim✘gangsta img/CVRTOON - İzmir Marşı.jpg",
-        path: "../../songs/vur yüreğim✘gangsta song/CVRTOON - İzmir Marşı.mp3"
+        img: "songs/vur yüreğim✘gangsta img/CVRTOON - İzmir Marşı.jpg",
+        path: "songs/vur yüreğim✘gangsta song/CVRTOON - İzmir Marşı.mp3"
     }
 ];
 
 const lofi = [
     {
         name: "Coke Studio Season 14",
-        img: "../../songs/lofi flip img/Coke Studio Season 14.png",
-        path: "../../songs/lofi flip song/Coke Studio  Season 14.mp3"
+        img: "songs/lofi flip img/Coke Studio Season 14.png",
+        path: "songs/lofi flip song/Coke Studio  Season 14.mp3"
     },
     {
         name: "LYRICAL - Main Agar Kahoon",
-        img: "../../songs/lofi flip img/LYRICAL - Main Agar Kahoon.jpg",
-        path: "../../songs/lofi flip song/LYRICAL Main Agar Kahoon.mp3"
+        img: "songs/lofi flip img/LYRICAL - Main Agar Kahoon.jpg",
+        path: "songs/lofi flip song/LYRICAL Main Agar Kahoon.mp3"
     },
     {
         name: "PANIYON SA - LoFi Remix - Anik",
-        img: "../../songs/lofi flip img/PANIYON SA - LoFi Remix - Anik8.jpeg",
-        path: "../../songs/lofi flip song/PANIYON SA - LoFi Remix - Anik8.mp3"
+        img: "songs/lofi flip img/PANIYON SA - LoFi Remix - Anik8.jpeg",
+        path: "songs/lofi flip song/PANIYON SA - LoFi Remix - Anik8.mp3"
     },
     {
         name: "Saibo Lofi Flip VIBIE",
-        img: "../../songs/lofi flip img/Saibo Lofi Flip VIBIE.jpg",
-        path: "../../songs/lofi flip song/Saibo Lofi Flip  VIBIE.mp3"
+        img: "songs/lofi flip img/Saibo Lofi Flip VIBIE.jpg",
+        path: "songs/lofi flip song/Saibo Lofi Flip  VIBIE.mp3"
     },
     {
         name: "Tere Vaaste Zara Hatke Zara Bachke",
-        img: "../../songs/lofi flip img/Tere Vaaste Zara Hatke Zara Bachke.jpg",
-        path: "../../songs/lofi flip song/Tere Vaaste  Zara Hatke Zara Bachke.mp3"
+        img: "songs/lofi flip img/Tere Vaaste Zara Hatke Zara Bachke.jpg",
+        path: "songs/lofi flip song/Tere Vaaste  Zara Hatke Zara Bachke.mp3"
     }
 ];
 
@@ -174,7 +174,7 @@ let innerHtml3 = "";
 const playSong = () => {
     isPlay = true;
     music.play();
-    play.src = "../../icons/pause.svg";
+    play.src = "icons/pause.svg";
     img.classList.add("rotted");
     navmusic.classList.add("rotted");
 };
@@ -182,7 +182,7 @@ const playSong = () => {
 const pauseSong = () => {
     isPlay = false;
     music.pause();
-    play.src = "../../icons/play.svg";
+    play.src = "icons/play.svg";
     img.classList.remove("rotted");
     navmusic.classList.remove("rotted");
 };
@@ -297,7 +297,7 @@ for (let i = 0; i < arijit.length; i++) {
             <div class="songslistbar">
                 <img src="${arijit[i].img}"/>
                 <p><a href="#">${arijit[i].name}</a></p>
-                <img src="../../icons/play-min.svg">
+                <img src="icons/play-min.svg">
             </div>
         `;
 }
@@ -310,7 +310,7 @@ for (let i = 0; i < Bollywood.length;i++) {
             <div class="songslistbar">
                 <img src="${Bollywood[i].img}"/>
                 <p><a href="#">${Bollywood[i].name}</a></p>
-                <img src="../../icons/play-min.svg">
+                <img src="icons/play-min.svg">
             </div>
         `;
 }
@@ -323,7 +323,7 @@ for (let i = 0; i < mysong.length; i++) {
             <div class="songslistbar">
                 <img src="${mysong[i].img}"/>
                 <p><a href="#">${mysong[i].name}</a></p>
-                <img src="../../icons/play-min.svg">
+                <img src="icons/play-min.svg">
             </div>
         `;
 }
@@ -335,7 +335,7 @@ songbox3.innerHTML = innerHtml3
 for (let i = 0; i < songslistbar.length; i++) {
      songslistbar[i].addEventListener("click", () => {
         if (tracks.length > 0) {
-            tracks[0].src = "../../icons/play-min.svg";
+            tracks[0].src = "icons/play-min.svg";
         }
 
         if (isPlays && songslistbar[i] == isPlays) {
@@ -345,7 +345,7 @@ for (let i = 0; i < songslistbar.length; i++) {
             songIndex = i;
             loadSong(songIndex);
             playSong();
-            songslistbar[i].lastElementChild.src = "../../icons/pause-min.svg";
+            songslistbar[i].lastElementChild.src = "icons/pause-min.svg";
             tracks[0] = songslistbar[i].lastElementChild;
 
             songBar.classList.remove("popup");
